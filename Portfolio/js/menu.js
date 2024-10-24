7c946432539175ada6c8be1b286faceb90f9bd7d
Yang <125204642+Yangder201@users.noreply.github.com>
2024-10-24 22:21:43 +0800

    Add files via upload

--- Portfolio/js/menu.js
+++ Portfolio/js/menu.js
@@ -2,27 +2,27 @@
 
   $.fn.menumaker = function(options) {
       
-      var cssmenu = $(this), settings = $.extend({
-        title: "",
-        format: "dropdown",
-        sticky: false
-      }, options);
+      // var cssmenu = $(this), settings = $.extend({
+      //   title: "",
+      //   format: "dropdown",
+      //   sticky: false
+      // }, options);
 
-      return this.each(function() {
-        cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
-        $(this).find("#menu-button").on('click', function(){
-          $(this).toggleClass('menu-opened');
-          var mainmenu = $(this).next('ul');
-          if (mainmenu.hasClass('open')) { 
-            mainmenu.hide().removeClass('open');
-          }
-          else {
-            mainmenu.show().addClass('open');
-            if (settings.format === "dropdown") {
-              mainmenu.find('ul').show();
-            }
-          }
-        });
+      // return this.each(function() {
+      //   cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
+      //   $(this).find("#menu-button").on('click', function(){
+      //     $(this).toggleClass('menu-opened');
+      //     var mainmenu = $(this).next('ul');
+      //     if (mainmenu.hasClass('open')) { 
+      //       mainmenu.hide().removeClass('open');
+      //     }
+      //     else {
+      //       mainmenu.show().addClass('open');
+      //       if (settings.format === "dropdown") {
+      //         mainmenu.find('ul').show();
+      //       }
+      //     }
+      //   });
 
         cssmenu.find('li ul').parent().addClass('has-sub');
 
